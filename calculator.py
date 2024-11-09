7f964543be72e249ba59f213da422fdc9ec86d4a
Sagargarg-7078 <sagargarg203209@gmail.com>
2024-11-09 17:36:57 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,15 @@
+first = int(input("enter the first number : "))
+operator = input("enter operator (+,-,/,,*) : ")
+second =int(input("enter the second number : "))
+if operator == "+":
+    print(first+second)
+elif operator == "-":
+    print(first-second)
+elif operator =="/":
+    print(first%second)
+elif operator=="*":
+    print(first*second)
+elif operator =="":
+    print(first**second)
+else:
+    print("Invalid operator")
